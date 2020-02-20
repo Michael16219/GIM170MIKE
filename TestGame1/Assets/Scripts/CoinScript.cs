8839690804724bfb2ff832e6d1850a56cdc13781
Michael16219 <61007828+Michael16219@users.noreply.github.com>
2020-02-20 20:04:40 +0800

    Week3

---
+++ TestGame1/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public float spinSpeed = 100f;
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0f, 0f, Time.deltaTime * this.spinSpeed);
+    }
+    private void onTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
+}
